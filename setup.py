66ffc9daf1b3da510cc358c89fabebae650656d8
Luca Grillotti <lg4615@ic.ac.uk>
2024-09-22 17:49:31 +0100

    adding install requires to setup.py

--- setup.py
+++ setup.py
@@ -20,22 +20,17 @@ setup(
     description="A-maze-ing environment in jax",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # install_requires=[
-    #     "absl-py>=1.0.0",
-    #     "jax>=0.3.16",
-    #     "jaxlib>=0.3.15",  # necessary to build the doc atm
-    #     "jinja2<3.1.0",
-    #     "jumanji>=0.1.3",
-    #     "flax>=0.6, <0.6.2",
-    #     "brax>=0.0.15",
-    #     "gym>=0.23.1",
-    #     "numpy>=1.22.3",
-    #     "scikit-learn>=1.0.2",
-    #     "scipy>=1.8.0",
-    # ],
-    # dependency_links=[
-    #     "https://storage.googleapis.com/jax-releases/jax_releases.html",
-    # ],
+    install_requires=[
+        "jax>=0.4.28",
+        "jaxlib>=0.4.28",
+        "flax>=0.8.5",
+        "qdax>=0.4.0",
+        "tqdm>=4.66.5",
+        "imageio>=2.35.1",
+    ],
+    extras_require={
+        "cuda12": ["jax[cuda12]>=0.4.30"],
+    },
     keywords=["Quality-Diversity", "Reinforcement Learning", "JAX"],
     classifiers=[
         "Development Status :: 4 - Beta",
