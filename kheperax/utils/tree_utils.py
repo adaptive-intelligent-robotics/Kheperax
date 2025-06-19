052be06902baaf70cb0149e9058e3541edaf02e4
Bryon Tjanaka <38124174+btjanaka@users.noreply.github.com>
2025-06-19 05:19:01 -0700

    Replace call to jax.tree_leaves (#2)

--- kheperax/utils/tree_utils.py
+++ kheperax/utils/tree_utils.py
@@ -3,7 +3,7 @@ from chex import ArrayTree
 
 
 def get_batch_size(tree: ArrayTree) -> int:
-    batch_size = jax.tree_leaves(tree)[0].shape[0]
+    batch_size = jax.tree_util.tree_leaves(tree)[0].shape[0]
     return batch_size
 
 
